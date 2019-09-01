8f403efa592571cebfe80f6456b1ca552714982d
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-01 13:39:51 -0300

    Clase 10 - Datos Numéricos

---
+++ tipos_de_datos.py
@@ -0,0 +1,19 @@
+# En python tenemos 2 Tipos de datos "NUMÉRICOS y CADENAS" aunque también
+# podríamos decir que existen los Booleanos
+
+# NUMÉRICOS
+# Enteros (int)
+entero = 10
+
+# Flotantes (float)
+flotante = 10.05
+
+# Complejos (complex)
+complejo = (10.05+0j)
+
+# BOOLEANOS (bool)
+# Verdadero (True) y Falso (False)
+
+
+# CADENA DE TEXTO (str)
+
