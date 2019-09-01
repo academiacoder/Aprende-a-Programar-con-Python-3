31f41fb57a3e95ae4127aafa4b512e69232f2d03
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-01 15:27:47 -0300

    Clase 11 Datos Booleanos

--- tipos_de_datos.py
+++ tipos_de_datos.py
@@ -4,16 +4,43 @@
 # NUMÉRICOS
 # Enteros (int)
 entero = 10
+# Convierte a un entero
+int(10.05)
 
 # Flotantes (float)
 flotante = 10.05
+# Convierte a flotante
+float(10)
 
 # Complejos (complex)
 complejo = (10.05+0j)
+# Convierte a complejo
+complex(10)
+
 
 # BOOLEANOS (bool)
 # Verdadero (True) y Falso (False)
+verdadero = True
+falso = False
+# Retornar el valor booleano de un dato
+bool(0)  # False
+bool(2)  # True
+bool(-2)  # True
+bool(0.0)  # False
+bool(0.1)  # True
+bool("")  # False
+bool("Hola")  # True
 
 
 # CADENA DE TEXTO (str)
+
+
+
+
+
+
+
+
+
+
 
