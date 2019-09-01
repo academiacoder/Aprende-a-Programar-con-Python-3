298f533537f88c42917420918f1638896ee8763c
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-01 02:11:17 -0300

    Clase 09 - Variables

---
+++ variables.py
@@ -0,0 +1,20 @@
+# Una variable es un espacio en memoria capaz de almacenar información
+
+# El operador de asignación es el simbolo = y nos permite asignar un valor a una variable
+
+# Para asignar una variable lo hacemos de esta forma
+numero = 10
+
+# Podemos asignar varios valores a diferentes variables en una única linea
+# separando los valores con comas
+# Debe contener el mismo número de elementos antes y después del igual
+numero1, numero2, numero3 = 1, 2, 3
+
+# La función id() nos permite ver el identificador único de la variable
+id(numero)
+
+# La función del() nos permite eliminar de memoria una variable
+del(numero)
+
+# dir() nos permite ver todo el listado de nombres asignados
+dir()
