cde57cc5c944ef7d35b82c7372c4b02a2d473b10
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-01 21:20:11 -0300

    Clase 15

--- operadores.py
+++ operadores.py
@@ -29,10 +29,66 @@ numero2 = 5
 # Realizar todas las operaciones aritméticas utilizando estos 2 números!
 
 resultado = 12 * 5 + 2 / 3 ** 2
-print(resultado)
+# print(resultado)
 
 resultado1= (12 * 5) + (2 / (3 ** 2))
-print(resultado1)
+# print(resultado1)
 
 resultado2 = (12 * 5) + (2 / 3) ** 2
-print(resultado2)
+# print(resultado2)
+
+
+# OPERADORES PARA CADENAS
+"""
++ Operador de Concatenación
+* Operador de Repetición
+"""
+
+cadena1 = "Hola "
+cadena2 = "mundo"
+
+cadenas_unidas = cadena1 + cadena2
+
+# print(cadenas_unidas)
+
+cadenas_repetidas = cadena1 * 3
+
+# print(cadenas_repetidas)
+
+
+# OPERADORES DE RELACIÓN
+# Los operadores de relación evaluan si 2 valores u objetos cumplen con una condición.
+# El resultado de dicha evaluación es un objeto booleano (bool)
+"""
+== ¿Igual A?
+!= ¿Distinto de?
+> ¿Mayor que?
+< ¿Menor que?
+>= ¿Mayor o Igual que?
+<= ¿Menor o Igual que?
+"""
+
+numero1 = 10
+numero2 = 5
+numero3 = 10.0
+numero4 = -10
+numero5 = 5
+numero_cadena = "10"
+
+igual = numero1 == numero2
+# Tarea hacer todas las comprobaciones con los distintos números y su respectivo print
+
+distinto = numero1 != numero2
+# Tarea hacer todas las comprobaciones con los distintos números y su respectivo print
+
+mayor_que = numero1 > numero2
+# Tarea hacer todas las comprobaciones con los distintos números y su respectivo print
+
+menor_que = numero1 < numero2
+# Tarea hacer todas las comprobaciones con los distintos números y su respectivo print
+
+mayor_igual = numero1 >= numero2e
+# Tarea hacer todas las comprobaciones con los distintos números y su respectivo print
+
+menor_igual = numero1 >= numero2
+# Tarea hacer todas las comprobaciones con los distintos números y su respectivo print
