80d9538daad539433f0524bf1e3952f361b7e5fe
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-02 11:05:18 -0300

    Clase 24 y 25 - Calculadora e Historia loca

--- 24_calculadora.py
+++ 24_calculadora.py
@@ -1,7 +1,17 @@
 # CALCULADORA BÁSICA - PROYECTO NRO 1
 # Solicitar al usuario 2 números y realizar la suma de ellos
 
+# Declaramos las variables
+# Utilizamos input para que se guarde lo que el usuario ingresa
+num1 = input("Ingresa un número: ")
+num2 = input("Ingresa otro número: ")
 
+# Realizamos la suma y guardamos el resultado
+res = float(num1) + float(num2)
+
+# Imprimimos el resultado y un mensaje de despedida
+print("La suma es:", res)
+print("GRACIAS!")
 
 # TAREA PARA EL ESTUDIANTE
 # Agregar a la calculadora el resto de las operaciones aritméticas disponibles en Python
--- 25_juego_historia_loca.py
+++ 25_juego_historia_loca.py
@@ -1,1 +1,21 @@
+# JUEGO HISTORIA LOCA
 # http://www.educandolectores.es/2018/07/02/mad-libs-o-historias-locas-un-juego-de-palabras-muy-divertido/
+
+print("CREA TU HISTORIA LOCA")
+print("Ingresa las palabras que te solicitamos:\n")
+# Declaramos las variables
+# utilizando input para que el usuario ingrese lo que pedimos
+pal1 = input("Parte del cuerpo en plural: ")
+pal2 = input("Adjetivo en plural: ")
+pal3 = input("Adjetivo: ")
+pal4 = input("Adjetivo: ")
+pal5 = input("Nombre propio: ")
+
+print("\nESTA ES LA HISTORIA QUE HAS CREADO:")
+# Impirimimos la historia por pantalla
+print(f"Antiguamente, el ser humano caminaba a cuatro {pal1.lower()},")
+print(f"se expresaba mediante gruñidos {pal2} y no sabía encender")
+print(f"un {pal3} fuego. Esta es la historia del día en que la humanidad")
+print(f"cambió para siempre (traducida del {pal4} idioma de las cavernas):")
+# TAREA PARA EL ESTUDIANTE: Completar la historia o inventar una nueva
+print(f"El nombre era {pal5.capitalize()}")  # Esta linea no pertece a la historia CAMBIALA!!!
