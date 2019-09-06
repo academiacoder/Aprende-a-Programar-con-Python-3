db58181e8733e3afd68912bb0d64a4c24a24ccfd
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-06 00:13:31 -0300

    Clase 26 - Listas

---
+++ 26_listas.py
@@ -0,0 +1,10 @@
+# LISTAS
+# Una lista es una colección de elementos, las listas están ordenadas, y son mutables.
+
+frutas = ["Naranja", "Manzana", "Banana", "Kiwi", "Mandarina", "Uva", "Naranja"]
+
+print(frutas)
+# print(frutas[5])
+# print(frutas[-6])
+# print(frutas[1:5])
+
