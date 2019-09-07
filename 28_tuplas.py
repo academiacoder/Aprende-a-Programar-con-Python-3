5b0bcebf239e35b7f6aab0638506c936594d03a3
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-06 22:28:36 -0300

    Clase 28 - Tuplas

---
+++ 28_tuplas.py
@@ -0,0 +1,10 @@
+# TUPLAS
+# Las tuplas son una colección de elementos ordenados al igual que las listas
+# pero las tuplas son inmutables.
+
+coordenadas = (42.88, 55.16)
+lista = [42.88, 55.16]
+
+coordenadas[0] = 5
+
+print(coordenadas)
---
+++ 29_funciones.py
@@ -0,0 +1,5 @@
+# FUNCIONES
+# Una función es un bloque de código que incluye instrucciones para realizar
+# una taréa específica
+
+
