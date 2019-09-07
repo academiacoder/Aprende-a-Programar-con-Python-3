a697e6bd57482dd39894af4454ad88d0359075a1
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-06 23:51:28 -0300

    Clase 30 - Lambda

--- 29_funciones.py
+++ 29_funciones.py
@@ -8,6 +8,15 @@ def saludar_usuario(par1, par2):
     print("Adios")
 
 
-print("primero")
-saludar_usuario("35", "Marcos")
-print("ultimo")
+# print("primero")
+# saludar_usuario("35", "Marcos")
+# print("ultimo")
+
+# FUNCIONES LAMBDA
+# Son funciones anónimas que se ejecutan en una linea es decir solo pueden tener una expresión
+
+resultado = lambda numero: numero + 20
+
+suma = lambda numero1, numero2: numero1 + numero2
+
+print(suma(30, 60))
