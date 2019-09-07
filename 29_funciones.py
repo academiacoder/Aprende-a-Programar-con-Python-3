2c1d11c038f4379046e4fa0f334fa5f1ec02b8f4
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-06 23:36:38 -0300

    Clase 29 - Funciones

--- 29_funciones.py
+++ 29_funciones.py
@@ -3,3 +3,11 @@
 # una taréa específica
 
 
+def saludar_usuario(par1, par2):
+    print("Hola " + par2 + " tienes " + par1 + " años")
+    print("Adios")
+
+
+print("primero")
+saludar_usuario("35", "Marcos")
+print("ultimo")
