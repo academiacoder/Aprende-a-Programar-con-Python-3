f683a6adbfcfb76e738cd1d8591695ac95865df1
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-06 21:22:53 -0300

    Clase 27 - Funciones de listas

--- 26_listas.py
+++ 26_listas.py
@@ -1,10 +1,18 @@
 # LISTAS
 # Una lista es una colección de elementos, las listas están ordenadas, y son mutables.
 
-frutas = ["Naranja", "Manzana", "Banana", "Kiwi", "Mandarina", "Uva", "Naranja"]
-
+numeros = [5, 2, 23, 55, 1, 9, 6]
+frutas = ["Naranja", "Manzana", "Banana", "Kiwi", "Mandarina", "Uva", "Naranja", "Kiwi"]
+print("LISTA ORIGINAL DE FRUTAS:")
 print(frutas)
+print()
 # print(frutas[5])
 # print(frutas[-6])
 # print(frutas[1:5])
 
+# FUNCIONES DE LAS LISTAS
+# https://docs.python.org/3/library/array.html
+# list ,len, append, extend, insert, remove, clear, pop, index, count, sort, reverse, copy
+frutas2 = frutas.copy()
+frutas3 = frutas
+print(frutas2)
