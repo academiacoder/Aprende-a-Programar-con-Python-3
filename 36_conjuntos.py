736ebece0d3baaf4be6bf0352133c29742b62130
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-08 15:24:02 -0300

    Clase 36 - Conjuntos

---
+++ 36_conjuntos.py
@@ -0,0 +1,16 @@
+# CONJUNTOS
+# Los conjuntos son una colección de datos pero que no se ecuentra ordenada.
+
+# [] Listas - () Tuplas - {} Conjuntos y Diccionarios
+
+conjunto = {"Hola", 2, False, 48.03, True}
+
+print(conjunto)
+
+conjunto.add(True)
+
+print(conjunto)
+
+conjunto.remove(True)
+
+print(conjunto)
