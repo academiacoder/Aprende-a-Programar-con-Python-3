68cec4ee680ecd3ba150ea91abfca0e3b8e236a8
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-08 18:05:25 -0300

    Clase 29 - For

---
+++ 39_for.py
@@ -0,0 +1,13 @@
+# FOR
+# Nos permite recorrer una colección de valores
+
+dias = ["lu", "ma", "mi"]
+
+for indice in range(10):
+    if indice == 4:
+        print("Ganaste el sorteo")
+    else:
+        print("No eres ganador")
+
+
+print(len(dias))
