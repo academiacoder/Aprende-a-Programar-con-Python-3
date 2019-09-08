3444cff6d4642ec19aeaaa5dbd80f9cb34ddb096
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-08 17:33:25 -0300

    Clase 38 - While

---
+++ 38_while.py
@@ -0,0 +1,13 @@
+# BUCLE WHILE
+# El bucle while nos permite ejecutar una accion repetidas veces
+# mientras una condición no se cumpla
+
+i = 1
+
+while i <= 5:
+    print(i)
+
+
+print("Termino el bucle")
+
+print(i)
