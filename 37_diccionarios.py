bd58aa88244eaeeabf321122d8e54ccebd7f43b4
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-08 16:13:56 -0300

    Clase 37 - Diccionarios

---
+++ 37_diccionarios.py
@@ -0,0 +1,17 @@
+# DICCIONARIOS
+# Los diccionarios son una colección de datos no ordenada, pero que almacena sus
+# datos utilizando una asignación (llave, valor).
+
+# [] Listas - () Tuplas - {} Conjuntos y Diccionarios
+
+dias = {
+    "lun": "Lunes",
+    "mar": "Martes",
+    "mie": "Miércoles",
+    "jue": "Jueves",
+    "vie": "Viernes",
+    "sab": "Sabado",
+    "dom": "Domingo"
+}
+
+print(dias["lun"])
