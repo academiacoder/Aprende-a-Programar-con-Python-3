ee277e111536fedc32e71d46a0d4d34450e0a619
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-13 08:50:00 -0300

    Clase 42 - Ciclos infinitos

---
+++ 42_ciclos_infinitos.py
@@ -0,0 +1,20 @@
+# CICLOS INFINITOS
+
+# Iniciamos un ciclo infinito
+while True:
+    # Solicitar al usuario 1 para continuar el ciclo
+    # 2 para detener el ciclo
+    print("Ingresa (1) para continuar")
+    print("Ingresa (2) para detener")
+    opcion = input(">")
+
+    if opcion == "1":
+        print("GRACIAS, selecciona otra opción")
+        print("---o---")
+    if opcion == "2":
+        print("GRACIAS, has detenido el ciclo")
+        break
+
+# TAREA, agrega un ciclo infinito al juego Adivina el Numero 2.0.
+# para que al finalizar te permita, elegir si volver a jugar
+# o te brinde un mensaje de despedida.
