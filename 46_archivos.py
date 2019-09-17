d641a6ce62ec67fdd991893c565f766157469e39
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-17 20:46:35 -0300

    Clase 48 - Archivos Escribir

--- 46_archivos.py
+++ 46_archivos.py
@@ -2,6 +2,8 @@
 
 # Abrir y Cerrar archivos
 
-archivo_estudiantes = open("estudiantes.txt", "r")  # Así abrimos un archivo
-print(archivo_estudiantes.read())
+archivo_estudiantes = open("estudiantes.txt", "a")  # Así abrimos un archivo
+print(archivo_estudiantes.write("\nCarolina Massutti - ReactJS"))
 archivo_estudiantes.close()  # Así cerramos un archivo
+
+# Eliminar un archivo
