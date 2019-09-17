3b500419a7a86e61883be42b29f1fabc3869387e
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-17 08:34:07 -0300

    Clase 47 - Leer Archivos

--- 46_archivos.py
+++ 46_archivos.py
@@ -3,5 +3,5 @@
 # Abrir y Cerrar archivos
 
 archivo_estudiantes = open("estudiantes.txt", "r")  # Así abrimos un archivo
-print(archivo_estudiantes)
+print(archivo_estudiantes.read())
 archivo_estudiantes.close()  # Así cerramos un archivo
