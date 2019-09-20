80b081d6f47516d1f4dcb87bdb4d6836ccc8415f
Marcos Guerrini <marcosguerrini@gmail.com>
2019-09-19 22:47:50 -0300

    Clase 52 - Pip

--- 50_modulos.py
+++ 50_modulos.py
@@ -1,8 +1,19 @@
+import random
+from docx import Document
 from utilidades_extra import tirar_dado as td
+
 # MODULOS
 
 # Un módulo es un archivo que podemos importar dentro de un archivo en el cual
 # estamos trabajando y hacer uso de su contenido, como funciones y variables.
 
 print(td(6))
+
+print(random.randint(10, 15))
+
+# PIP
+# Gestor de paquetes, gestor de módulos o librerias creadas por terceros.
+
+document = Document()
+
 
