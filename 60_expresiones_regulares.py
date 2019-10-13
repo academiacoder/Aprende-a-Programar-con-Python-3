02c42e5ab558a15daabefa378dcd780fd0940337
Marcos Guerrini <marcosguerrini@gmail.com>
2019-10-12 21:20:37 -0300

    Clase 61 - Expresiones Regulares 02

--- 60_expresiones_regulares.py
+++ 60_expresiones_regulares.py
@@ -22,23 +22,39 @@ import re
 # Este método nos buscará la primer coincidencia de una cadena de caracteres dentro de un texto
 # devolverá un objeto si es localizado o None si no es encontrado.
 
-cadena = "Hola estudiantes de AcademiaCoder."
+cadena = "Hola estudiantes de AcademiaCoder. En AcademiaCoder los estudiantes aprenderan programación."
+
+cadena2 = """
+Hola estudiantes de AcademiaCoder.
+En AcademiaCoder los estudiantes aprenderan programación.
+Recuerden que nos centramos en el Desarrollo Web
+"""
 
 a_buscar = "estudiantes"
 
 buscado = re.search(a_buscar, cadena)
 
+"""
 print(buscado)
 print(buscado.start())
 print(buscado.end())
 print(buscado.span())
 print(a_buscar)
+"""
+# Clase 2 - re.findall, re.split, re.sub
+# En esta clase aprenderemos a utilizar estos métodos de la librería re
+
+re_findall = re.findall(a_buscar, cadena)
+print(re_findall)
+
+re_split = re.split("\s", cadena)
+print(re_split)
+
+re_sub = re.sub(a_buscar, "desarrolladores", cadena)
+print(re_sub)
 
 
-
-
-
-
+print(cadena2)
 
 
 
